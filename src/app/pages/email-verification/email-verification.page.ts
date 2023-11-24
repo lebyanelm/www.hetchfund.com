6aae89ce6b495282b516f6c713bedc50e6658913
Libby Lebyane <lebyane.lm@gmail.com>
2023-11-24 12:49:13 +0000

    added code verification method

--- src/app/pages/email-verification/email-verification.page.ts
+++ src/app/pages/email-verification/email-verification.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ERROR_MESSAGES } from 'src/app/error_messages';
+import { RouterService } from 'src/app/services/router.service';
+import { SessionService } from 'src/app/services/session.service';
+import { ToastManagerService } from 'src/app/services/toast-manager.service';
+import { environment } from 'src/environments/environment';
+import * as superagent from 'superagent';
 
 @Component({
   selector: 'app-email-verification',
@@ -7,9 +14,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmailVerificationPage implements OnInit {
 
-  constructor() { }
+  hetcherId = null;
+  constructor(private activatedRoute: ActivatedRoute, private router: RouterService, private toastService: ToastManagerService, private sessionService: SessionService, private routerService: RouterService) { }
 
   ngOnInit() {
+    this.activatedRoute.queryParamMap.subscribe((queryParamMap) => {
+      this.hetcherId = atob(queryParamMap.get("hid"));
+      console.log(this.hetcherId)
+    })
   }
 
+  verifyCode(code) {
+    if (this.hetcherId) {
+      superagent
+        .get([environment.accounts, 'verifications', '2fa', 'code', code, this.hetcherId].join('/'))
+        .end((_, response) => {
+          if (response) {
+            if (response.statusCode === 200) {
+              // Store the JWT locally for future logins.
+              this.sessionService.setToken({
+                uid: this.hetcherId,
+                token: response.body.data.session_token,
+              });
+              this.routerService.route(['pitches']);
+            } else {
+              this.toastService.show(response.body.reason || ERROR_MESSAGES.UNEXPECTED_ERROR);
+            }
+          } else {
+            this.toastService.show(ERROR_MESSAGES.NO_INTERNET);
+          }
+        })
+    } else {
+      this.toastService.show("Something is wrong with the verification. Please start the signin process again.");
+    }
+  } 
 }
